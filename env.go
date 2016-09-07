68c8864b64c0efeb3af2bc75cc55033ad7772f04
Peter Hellberg <peter@c7.se>
2016-09-07 11:11:04 +0200

    Client interface (and client implementation)
    DefaultClient is using os.Getenv
    
    Func creates a func(string) string backed by a map

--- env.go
+++ env.go
@@ -41,9 +41,40 @@ import (
 	"time"
 )
 
+// DefaultClient is the default client backed by os.Getenv
+var DefaultClient = NewClient(os.Getenv)
+
+// Client is the env client interface
+type Client interface {
+	Bool(key string, fallback bool) bool
+	Bytes(key string, fallback []byte) []byte
+	Float64(key string, fallback float64) float64
+	Duration(key string, fallback time.Duration) time.Duration
+	Int(key string, fallback int) int
+	String(key, fallback string) string
+	Strings(key string, fallback []string, seps ...string) []string
+	URL(key string, fallback *url.URL) *url.URL
+}
+
+type client struct {
+	Getenv func(string) string
+}
+
+// NewClient creates a new Client backed by provided func(string) string
+func NewClient(f func(string) string) Client {
+	return &client{f}
+}
+
+// Func creates a func(string) string backed by a map[string]string
+func Func(v map[string]string) func(string) string {
+	return func(k string) string {
+		return v[k]
+	}
+}
+
 // Bool returns a bool from the ENV, or fallback variable
-func Bool(key string, fallback bool) bool {
-	if b, err := strconv.ParseBool(os.Getenv(key)); err == nil {
+func (c *client) Bool(key string, fallback bool) bool {
+	if b, err := strconv.ParseBool(c.Getenv(key)); err == nil {
 		return b
 	}
 
@@ -51,8 +82,8 @@ func Bool(key string, fallback bool) bool {
 }
 
 // Bytes returns a slice of bytes from the ENV, or fallback variable
-func Bytes(key string, fallback []byte) []byte {
-	if v := os.Getenv(key); v != "" {
+func (c *client) Bytes(key string, fallback []byte) []byte {
+	if v := c.Getenv(key); v != "" {
 		return []byte(v)
 	}
 
@@ -60,8 +91,8 @@ func Bytes(key string, fallback []byte) []byte {
 }
 
 // Float64 returns a float64 from the ENV, or a fallback variable
-func Float64(key string, fallback float64) float64 {
-	if f, err := strconv.ParseFloat(os.Getenv(key), 64); err == nil {
+func (c *client) Float64(key string, fallback float64) float64 {
+	if f, err := strconv.ParseFloat(c.Getenv(key), 64); err == nil {
 		return f
 	}
 
@@ -69,8 +100,8 @@ func Float64(key string, fallback float64) float64 {
 }
 
 // Duration returns a duration from the ENV, or fallback variable
-func Duration(key string, fallback time.Duration) time.Duration {
-	if d, err := time.ParseDuration(os.Getenv(key)); err == nil {
+func (c *client) Duration(key string, fallback time.Duration) time.Duration {
+	if d, err := time.ParseDuration(c.Getenv(key)); err == nil {
 		return d
 	}
 
@@ -78,8 +109,8 @@ func Duration(key string, fallback time.Duration) time.Duration {
 }
 
 // Int returns an int from the ENV, or fallback variable
-func Int(key string, fallback int) int {
-	if i, err := strconv.Atoi(os.Getenv(key)); err == nil {
+func (c *client) Int(key string, fallback int) int {
+	if i, err := strconv.Atoi(c.Getenv(key)); err == nil {
 		return i
 	}
 
@@ -87,8 +118,8 @@ func Int(key string, fallback int) int {
 }
 
 // String returns a string from the ENV, or fallback variable
-func String(key, fallback string) string {
-	if v := os.Getenv(key); v != "" {
+func (c *client) String(key, fallback string) string {
+	if v := c.Getenv(key); v != "" {
 		return v
 	}
 
@@ -96,8 +127,8 @@ func String(key, fallback string) string {
 }
 
 // Strings returns a slice of strings from the ENV, or fallback variable
-func Strings(key string, fallback []string, seps ...string) []string {
-	if v := os.Getenv(key); v != "" {
+func (c *client) Strings(key string, fallback []string, seps ...string) []string {
+	if v := c.Getenv(key); v != "" {
 		sep := ","
 
 		if len(seps) > 0 {
@@ -111,8 +142,8 @@ func Strings(key string, fallback []string, seps ...string) []string {
 }
 
 // URL returns a URL from the ENV, or fallback URL if missing/invalid
-func URL(key string, fallback *url.URL) *url.URL {
-	if v := os.Getenv(key); v != "" {
+func (c *client) URL(key string, fallback *url.URL) *url.URL {
+	if v := c.Getenv(key); v != "" {
 		u, err := url.Parse(v)
 		if err != nil {
 			return fallback
@@ -122,4 +153,44 @@ func URL(key string, fallback *url.URL) *url.URL {
 	}
 
 	return fallback
+}
+
+// Bool returns a bool from the ENV, or fallback variable
+func Bool(key string, fallback bool) bool {
+	return DefaultClient.Bool(key, fallback)
+}
+
+// Bytes returns a slice of bytes from the ENV, or fallback variable
+func Bytes(key string, fallback []byte) []byte {
+	return DefaultClient.Bytes(key, fallback)
+}
+
+// Float64 returns a float64 from the ENV, or a fallback variable
+func Float64(key string, fallback float64) float64 {
+	return DefaultClient.Float64(key, fallback)
+}
+
+// Duration returns a duration from the ENV, or fallback variable
+func Duration(key string, fallback time.Duration) time.Duration {
+	return DefaultClient.Duration(key, fallback)
+}
+
+// Int returns an int from the ENV, or fallback variable
+func Int(key string, fallback int) int {
+	return DefaultClient.Int(key, fallback)
+}
+
+// String returns a string from the ENV, or fallback variable
+func String(key, fallback string) string {
+	return DefaultClient.String(key, fallback)
+}
+
+// Strings returns a slice of strings from the ENV, or fallback variable
+func Strings(key string, fallback []string, seps ...string) []string {
+	return DefaultClient.Strings(key, fallback, seps...)
+}
+
+// URL returns a URL from the ENV, or fallback URL if missing/invalid
+func URL(key string, fallback *url.URL) *url.URL {
+	return DefaultClient.URL(key, fallback)
 }
